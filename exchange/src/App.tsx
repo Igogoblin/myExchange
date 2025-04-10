3dbc35d6b56fc25558a0a285bb9c3dcef30cf360
Ihar Skavysh <igogoblin@gmail.com>
2025-04-10 15:16:46 +0300

    feat: add base layout

--- exchange/src/App.tsx
+++ exchange/src/App.tsx
@@ -1,11 +1,38 @@
 import "./App.css";
-
 import InputShow from "./components/input/InputShow";
 
 function App() {
   return (
     <section>
       <h1>Конвертор валют</h1>
+      <p>
+        по умолчанию ввод в суммах, для перевода в другую валюту нажмите
+        соответствующую кнопку
+      </p>
+      <div className="forInput">
+        <InputShow />
+        <button>Конвертировать</button>
+      </div>
+      <div className="orderBlock">
+        <button value={"USD"} className="choseMoney">
+          USD
+        </button>
+        <button value={"EUR"} className="choseMoney">
+          EUR
+        </button>
+        <button value={"RUB"} className="choseMoney">
+          RUB
+        </button>
+        <button value={"BYN"} className="choseMoney">
+          BYN
+        </button>
+      </div>
+      <div className="showValue">
+        <div>курс USD to UZB - </div>
+        <div className="correlation"></div>
+        <div> итого: </div>
+        <div className="result"></div>
+      </div>
     </section>
   );
 }
--- exchange/src/components/input/InputShow.tsx
+++ exchange/src/components/input/InputShow.tsx
@@ -1,9 +1,9 @@
 const InputShow = () => {
   return (
     <input
-      type="text"
+      type="number"
       className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 height-32"
-      placeholder="Enter text"
+      placeholder="Enter ..."
     />
   );
 };
